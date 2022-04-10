7c11bd5c7d344842b5341fb8ba6daab4d8646fdd
jhonboy121 <alfredmathew05@gmail.com>
2022-04-11 02:07:34 +0530

    ThemePicker: add support for custom themed icon pack [2/3]
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- src/com/android/customization/module/DefaultCustomizationSections.java
+++ src/com/android/customization/module/DefaultCustomizationSections.java
@@ -17,6 +17,7 @@ import com.android.customization.model.iconpack.IconPackSectionController;
 import com.android.customization.model.mode.DarkModeSectionController;
 import com.android.customization.model.mode.DarkModeSnapshotRestorer;
 import com.android.customization.model.theme.OverlayManagerCompat;
+import com.android.customization.model.themedicon.ThemedIconPackSectionController;
 import com.android.customization.model.themedicon.ThemedIconSectionController;
 import com.android.customization.model.themedicon.ThemedIconSwitchProvider;
 import com.android.customization.model.themedicon.domain.interactor.ThemedIconInteractor;
@@ -207,7 +208,7 @@ public final class DefaultCustomizationSections implements CustomizationSections
             CustomizationSectionNavigationController sectionNavigationController,
             @Nullable Bundle savedInstanceState,
             DisplayUtils displayUtils) {
-        List<CustomizationSectionController<?>> sectionControllers = new ArrayList<>();
+       final List<CustomizationSectionController<?>> sectionControllers = new ArrayList<>();
 
         // Wallpaper section.
         sectionControllers.add(
@@ -238,6 +239,12 @@ public final class DefaultCustomizationSections implements CustomizationSections
                 mThemedIconInteractor,
                 savedInstanceState,
                 mThemedIconSnapshotRestorer));
+
+        // Custom themed icon pack section.
+        sectionControllers.add(new ThemedIconPackSectionController(
+                activity, sectionNavigationController,
+                ThemedIconSwitchProvider.getInstance(activity),
+                lifecycleOwner, savedInstanceState));
 
         // App grid section.
         sectionControllers.add(
